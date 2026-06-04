f05c96e1302bafe974c05ee6337038c0b2ffa26b
nichedotsol <nichedotsol@gmail.com>
2026-06-03 20:39:24 -0400

    Serve SVG logo with image MIME type

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const types = {
   ".html": "text/html; charset=utf-8",
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
+  ".svg": "image/svg+xml; charset=utf-8",
 };
 
 http
